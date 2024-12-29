802648c073e7f96661791ed70a73579a703b5be0
Rachidmlk <rascid.seoul@gmail.com>
2024-12-29 20:49:08 +0100

    neinth commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,28 @@
-import Image from "next/image";
+'use client'
+import Header from "../components/Header";
+import Loader from "../components/Loader";
+import Play from "../components/Play";
+import Work from "../components/Work";
+import Lenis from "lenis";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [work, setWork] = useState<boolean>(true)
+  useEffect(()=>{
+    const lenis = new Lenis()
+    function raf(time: any){
+      lenis.raf(time)
+      requestAnimationFrame(raf)
+    }
+    requestAnimationFrame(raf)
+
+  }, [])
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="h-[1900px]">
+      <Loader/>
+      <Header setWork={setWork}/>
+      {work && <Work />}
+      {!work && <Play />}
       
     </div>
   );
