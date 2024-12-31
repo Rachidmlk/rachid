e433fade8bba3737ae278ca1567f33d502851cbd
Rachidmlk <rascid.seoul@gmail.com>
2024-12-31 09:24:32 +0100

    eiteenth commit

--- src/components/Cursus.tsx
+++ src/components/Cursus.tsx
@@ -12,6 +12,7 @@ function Cursus(props: any) {
     const textRef = useRef(null)
     const containerRef = useRef(null)
     useGSAP(()=>{
+        
         gsap.from(containerRef.current, {
             width: '4500px',
             margin: 'auto',
@@ -22,6 +23,7 @@ function Cursus(props: any) {
                 scrub: true
               }
         })
+        if(window.innerWidth < 640) return
         gsap.from(imageRef.current, {
             opacity: 0,
             scrollTrigger: {
@@ -33,11 +35,11 @@ function Cursus(props: any) {
         })
     })
   return (
-    <div ref={containerRef} className='w-[1500px] m-auto flex justify-between items-center px-52 mb-16'>
+    <div ref={containerRef} className='w-[1500px] m-auto flex max-sm:gap-8 max-sm:flex-col max-sm:w-[500px]  max-sm:justify-center justify-between items-center px-52 mb-16'>
       <div  >
-        <Image ref={imageRef} src={props.src} alt='bio' width={500} height={480} className=''/>
+        <Image ref={imageRef} src={props.src} alt='bio' width={500} height={480} className='max-sm:w-[500px] max-sm:h-[100px]'/>
       </div>
-      <div className='w-[500px] bg[#e3e3e3] rounded-lg p-2 text-xl text-justify text-sky-950'>
+      <div className='w-[500px] bg[#e3e3e3] rounded-lg p-2 text-xl text-justify text-sky-950 max-sm:text-sm max-sm:w-[400px]'>
       {props.text}
       </div>
     </div>
